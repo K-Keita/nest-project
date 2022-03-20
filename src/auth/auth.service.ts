bcf5a4f87c20272464d39a2eb30dbcc79221bc4a
kitagawakeita <k.syarin610@gmail.com>
2022-03-20 18:02:13 +0900

    authModuleの作成

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersServece: UsersService,
+  ) {}
+
+  async validateUser({ username, password }: CreateUserDto) {
+    const user = await this.usersServece.findOne(username);
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return isValid;
+  }
+  async login(user: CreateUserDto) {
+    if (await this.validateUser(user)) {
+      const payload = { username: user.username };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    }
+  }
+}
