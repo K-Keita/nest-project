55a973ec3825abde3bcf303a774ebfc15de0139a
kitagawakeita <k.syarin610@gmail.com>
2022-03-20 18:23:14 +0900

    環境変数を扱えるように編集

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.MONGODB_API}`, `.env`],
+    }),
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODB_API),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
